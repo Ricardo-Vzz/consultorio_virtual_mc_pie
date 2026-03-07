1e6877f7a86408b7ad099e2819459e7d966c98ec
thericardito <razquezv8@gmail.com>
2026-03-07 17:14:05 -0600

    Navbar completo

--- src/components/nav_bar.jsx
+++ src/components/nav_bar.jsx
@@ -5,10 +5,10 @@ function Navbar () {
    <nav className="nav">
     <div className="nav-brand">McPie</div>
        <ul className="nav-list">
-        <li><Link to="/">Home</Link></li>
+        <li><Link to="/">Inicio</Link></li>
         <li><Link to="/cita">Agendar Cita</Link></li>
         <li><Link to="/servicios">Servicios</Link></li>
-        {/*<li><Link to="/portafolio">Portafolio</Link></li>*/}
+        <li><Link to="/portafolio">Portafolio</Link></li>
         <li><Link to="/faq">Preguntas Frecuentes</Link></li>
         <li><Link to="/contacto">Contacto</Link></li>
        </ul>
